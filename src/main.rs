472b02d19d2482d2d236bc733835710de8cdcadc
Jason Washburn <jason@shyftsolutions.io>
2022-02-04 04:56:56 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use http::request::Request;
+use server::Server;
+
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+mod server {
+    pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr)
+        }
+    }
+}
+
+mod http {
+    pub mod request {
+        use super::method::Method
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
+        }
+    }
+    mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATH,
+        }
+    }
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
